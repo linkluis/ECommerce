c3fffd158548ac29ab352ff687bc4b53bd23d02d
Ext_luis.nunez <droidlinkluis@gmail.com>
2023-06-15 19:33:00 -0700

    Add existing project files to Git

---
+++ ECommerce.Api.Products/Providers/ProductsProvider.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using ECommerce.Api.Products.Db;
+using ECommerce.Api.Products.Db.Models;
+using ECommerce.Api.Products.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Products.Providers
+{
+    public class ProductsProvider : IProductsProvider
+    {
+
+        private readonly ProductsDbContext dbContext;
+        private readonly ILogger<ProductsProvider> logger;
+        private readonly IMapper mapper;
+
+        public ProductsProvider(ProductsDbContext dbContext, ILogger<ProductsProvider> logger, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.logger = logger;
+            this.mapper = mapper;
+            SeedData();
+        }
+
+        private void SeedData()
+        {
+            if (!dbContext.Products.Any())
+            {
+                dbContext.Add(new Db.Product() { Id = 1, Name = "Keyboard", Price = 20, Inventory = 100 });
+                dbContext.Add(new Db.Product() { Id = 2, Name = "Mouse", Price = 5, Inventory = 200 });
+                dbContext.Add(new Db.Product() { Id = 3, Name = "Monitor", Price = 150, Inventory = 1000 });
+                dbContext.Add(new Db.Product() { Id = 4, Name = "CPU", Price = 200, Inventory = 2000 });
+                dbContext.SaveChanges();
+            }
+        }
+
+        public async Task<(bool IsSuccess, IEnumerable<Db.Models.Product> Products,
+            string ErrorMessage)> GetProductsAsync()
+        {
+            try
+            {
+                var products = await dbContext.Products.ToListAsync();
+                if (products != null && products.Any()) {
+                    var result = mapper.Map<IEnumerable<Db.Product>, IEnumerable<Db.Models.Product>>(products);
+                    return (true, result, null);
+
+                }
+                return (false, null, "Not Result");
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+
+        public async Task<(bool IsSuccess, Db.Models.Product Product, string ErrorMessage)> GetProductsAsync(int id)
+        {
+            try
+            {
+                var product = await dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+                if (product != null)
+                {
+                    var result = mapper.Map<Db.Product, Db.Models.Product>(product);
+                    return (true, result, null);
+
+                }
+                return (false, null, "Not Result");
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+    }
+}
